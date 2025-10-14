1fc834c08d01953f746ff135a12d902fbc9700b9
Lê Nguyễn Chí Bảo <baolncse183721@fpt.edu.vn>
2025-10-14 12:15:01 +0700

    Create basic screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,32 +27,24 @@ export default function TabLayout() {
         headerShown: useClientOnlyValue(false, true),
       }}>
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
+        name="HomeScreen"
+        options={{title: 'Home', tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />}}
       />
       <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
+        name="ChatbotScreen"
+        options={{title: 'Chatbot', tabBarIcon: ({ color }) => <TabBarIcon name="comments" color={color} />}}
+      />
+      <Tabs.Screen
+        name="AnalyzeScreen"
+        options={{title: 'Analyze', tabBarIcon: ({ color }) => <TabBarIcon name="line-chart" color={color} />}}
+      />
+      <Tabs.Screen
+        name="CartScreen"
+        options={{title: 'Cart', tabBarIcon: ({ color }) => <TabBarIcon name="shopping-cart" color={color} />}}
+      />
+      <Tabs.Screen
+        name="ScheduleScreen"
+        options={{title: 'Schedule', tabBarIcon: ({ color }) => <TabBarIcon name="calendar" color={color} />}}
       />
     </Tabs>
   );
